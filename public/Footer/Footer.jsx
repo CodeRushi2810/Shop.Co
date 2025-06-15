04854d96b1fc9b22ae25c598accda040febb80bf
Rushi <patelrushi2810@gmail.com>
2025-06-15 10:53:36 +0530

    Bug Fixes

--- public/Footer/Footer.jsx
+++ public/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer({ mockData }) {
                 <div className='py-16 container mx-auto px-4'>
                     <div className="grid grid-cols-12">
                         <div className="w-full col-span-12 lg:col-span-3">
-                            <h1 className="heading-lg font-black mb-3">{mockData.logo}</h1>
+                            <h1 className="heading-lg font-black mb-3"><a href="http://" target="_blank" rel="noopener noreferrer"> {mockData.logo}</a></h1>
                             <p className="paragraph mb-4 text-[#666] font-normal">{mockData.subHeading}</p>
                         </div>
                         <div className="w-full col-span-12 grid grid-cols-12 lg:col-start-5 lg:col-span-8">
@@ -14,7 +14,7 @@ function Footer({ mockData }) {
                                 <ul key={index} className="col-span-6 mb-6 md:col-span-4 lg:col-span-3">
                                     <li className="text-black paragraph font-semibold mb-3 tracking-widest">{item.title}</li>
                                     {item.items.map((item, index) => (
-                                        <li key={index} className="text-[#666] paragraph font-normal mb-2">{item.name}</li>
+                                        <li key={index} className="text-[#666] paragraph font-normal mb-2"><a href="http://" target="_blank" rel="noopener noreferrer">{item.name}</a></li>
                                     ))}
                                 </ul>
                             ))}
@@ -22,11 +22,12 @@ function Footer({ mockData }) {
                     </div>
 
                     <div className="w-full border-b mb-6"></div>
+
                     <div className="w-full flex flex-col md:flex-row md:justify-between items-center">
                         <p className="text-center mb-4 text-[#666] paragraph font-normal">{mockData.footerText}</p>
                         <div className="flex justify-center">
                             {mockData.paymentMethods.map((item, index) => (
-                                <img key={index} src={item.logo} alt={item.name} />
+                                <a href="http://" target="_blank" rel="noopener noreferrer"><img key={index} src={item.logo} alt={item.name} /></a>
                             ))}
                         </div>
                     </div>
